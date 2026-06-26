565bcb755a88ee517f1f58ffd68c3288e7276e09
Marcin4568 <marcin.nowicki@edu-kw1c.nl>
2026-06-26 19:28:57 +0200

    update

--- progressie.js
+++
@@ -1,6 +0,0 @@
-const nutrition = JSON.parse(localStorage.getItem("nutrition"));
-
-if (nutrition) {
-    document.getElementById("data").innerText =
-        `Kcal: ${nutrition.kcal}, Eiwit: ${nutrition.protein}, Vet: ${nutrition.fat}, Carbs: ${nutrition.carbs}`;
-}
